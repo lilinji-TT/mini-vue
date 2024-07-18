15986618afad9ee0fa9279569524d42145620b94
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-07-18 21:56:24 +0800

    完成isReadonly，isReactive以及单测

--- src/reactivity/baseHandler.ts
+++ src/reactivity/baseHandler.ts
@@ -1,9 +1,17 @@
 import { track, trigger } from "./effect";
+import { ReactiveFlags } from "./reactive";
 const get = createGetter();
 const set = createSetter();
 const readonlyGet = createGetter(true);
+
 function createGetter(isReadonly = false) {
   return function (target, key) {
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+
     const res = Reflect.get(target, key);
 
     //TODO: 依赖收集
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,11 +1,24 @@
 import { mutableHandlers, readOnlyHandlers } from "./baseHandler";
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadOnly",
+}
+
 export function reactive(raw) {
   return createActiveObject(raw, mutableHandlers);
 }
 
 export function readonly(raw) {
   return createActiveObject(raw, readOnlyHandlers);
+}
+
+export function isReactive(value) {
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  return !!value[ReactiveFlags.IS_READONLY];
 }
 
 function createActiveObject(raw: any, baseHandler) {
