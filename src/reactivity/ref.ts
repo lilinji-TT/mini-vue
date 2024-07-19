c09a7293cd6e9a0cd10b252a020f4ff3b9729896
lilinji-TT <112566451+lilinji-TT@users.noreply.github.com>
2024-07-20 01:01:04 +0800

    实现proxyRefs

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -2,10 +2,15 @@ import { hasChanged, isObject } from "../shared";
 import { isTracking, trackEffects, triggerEffects } from "./effect";
 import { reactive } from "./reactive";
 
+/**
+ * 针对基于proxy来实现响应式系统的依赖追踪，ref定位是接收一个具体的值也就是 1 "1" true 这种，但是proxy代理
+ * 针对的是对象，所以需要包一层，然后使用.value的方式来匹配Proxy
+ */
 class RefImpl {
   private _value: any;
   private _rawValue: any;
   public deps: Set<any>;
+  public __v_isRef = true;
   constructor(value) {
     this._rawValue = value;
     this._value = convert(value);
@@ -29,6 +34,29 @@ class RefImpl {
 
 export function ref(value) {
   return new RefImpl(value);
+}
+
+export function isRef(ref) {
+  return !!ref.__v_isRef;
+}
+
+export function unRef(ref) {
+  return isRef(ref) ? ref.value : ref;
+}
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      return unRef(Reflect.get(target, key));
+    },
+    set(target, key, value) {
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value);
+      } else {
+        return Reflect.set(target, key, value);
+      }
+    },
+  });
 }
 
 function trackRefValue(ref) {
